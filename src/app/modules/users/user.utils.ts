69faf78ee725f600658eca3d04e214a0e29a6e1a
owakeelAhmmed <owakeel.ahmmed@gmail.com>
2023-06-02 00:26:11 +0600

    Create user service

---
+++ src/app/modules/users/user.utils.ts
@@ -0,0 +1,16 @@
+import { User } from './users.model'
+
+export const findLastUerId = async () => {
+  const lastUser = await User.findOne({}, { id: 1, _id: 0 })
+    .sort({
+      createdAt: -1,
+    })
+    .lean()
+  return lastUser?.id
+}
+
+export const generateUserId = async () => {
+  const currentId = (await findLastUerId()) || (0).toString().padStart(5, '0')
+  const incrementedId = currentId + 1
+  return incrementedId
+}
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-
-
-
-
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`🛢  Database is connected successfully`)
 
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log(`💽 Database is connected successfully`);
-
-
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-        });
-    } catch (err) {
-        console.log(`Failed to connect Database`, err);
-    };
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log(`Failed to connect Database`, err)
+  }
 }
 
-bootstrap();
+bootstrap()
